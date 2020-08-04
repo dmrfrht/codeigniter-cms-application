8b94a3db8c858bf728bce7419e13fe061bffd188
Ferhat Demir <dmrfrht1@gmail.com>
2020-08-04 23:25:41 +0300

    urun update islemi gerceklestirildi

--- panel/application/views/product_v/update/content.php
+++ panel/application/views/product_v/update/content.php
@@ -8,7 +8,7 @@
   <div class="col-md-12">
     <div class="widget">
       <div class="widget-body">
-        <form action="<?= base_url("product/save") ?>" method="post">
+        <form action="<?= base_url("product/update/$item->id") ?>" method="post">
           <div class="form-group">
             <label>Ürün Başlığı</label>
             <input type="text" name="title" class="form-control" placeholder="Başlık" value="<?= $item->title ?>">
@@ -24,7 +24,7 @@
               data-options="{height: 250}"
               name="description"><?= $item->description ?></textarea>
           </div>
-          <button type="submit" class="btn btn-primary btn-md btn-outline">Kaydet</button>
+          <button type="submit" class="btn btn-primary btn-md btn-outline">Güncelle</button>
           <a href="<?= base_url("product") ?>" class="btn btn-outline btn-md btn-danger">İptal</a>
         </form>
       </div><!-- .widget-body -->
