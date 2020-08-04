55f0f0b0c8396ff9f6883eea103c6562ce1ed325
Ferhat Demir <dmrfrht1@gmail.com>
2020-08-04 16:39:00 +0300

    urunler modulunun urun listesi icin html sablonu olusturuldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,35 @@
-Product Page
+<div class="row">
+  <div class="col-md-12">
+    <h4 class="m-b-lg">Ürün Listesi</h4>
+  </div>
+
+  <div class="col-md-12">
+    <div class="widget p-lg">
+
+      <table class="table table-hover table-striped table-bordered">
+        <thead>
+        <th>#id</th>
+        <th>url</th>
+        <th>Başlık</th>
+        <th>Açıklama</th>
+        <th>Durumu</th>
+        <th>İşlem</th>
+        </thead>
+        <tbody>
+        <tr>
+          <td>1</td>
+          <td>https://www.google.com</td>
+          <td>Monitör Askısı</td>
+          <td>Monitör Askısı Çok Güzel Hacı ya</td>
+          <td>1</td>
+          <td>
+            <a href="#" class="btn btn-danger btn-xs btn-outline"><i class="fa fa-trash"></i> Sil</a>
+            <a href="#" class="btn btn-info btn-xs btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
+          </td>
+        </tr>
+
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
