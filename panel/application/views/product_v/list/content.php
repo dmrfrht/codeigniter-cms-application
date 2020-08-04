5651f94b45788b5dd84b3cfcb8d8bfa6a180e5d3
Ferhat Demir <dmrfrht1@gmail.com>
2020-08-04 16:49:30 +0300

    toggle button eklendi. yeni ekle butonu eklendi ve kayit yoktur alert paneli eklendi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,10 +1,17 @@
 <div class="row">
   <div class="col-md-12">
-    <h4 class="m-b-lg">Ürün Listesi</h4>
+    <h4 class="m-b-lg">
+      Ürün Listesi
+      <a href="#" class="btn pull-right btn-outline btn-success btn-xs"><i class="fa fa-plus"></i> Yeni Ekle</a>
+    </h4>
   </div>
 
   <div class="col-md-12">
     <div class="widget p-lg">
+
+      <div class="alert alert-info text-center">
+        <p>Burada herhangi bir veri kaydı bulunmamaktadır. Eklemek için lütfen <a href="#" class="">tıklayınız</a>.</p>
+      </div>
 
       <table class="table table-hover table-striped table-bordered">
         <thead>
@@ -16,12 +23,21 @@
         <th>İşlem</th>
         </thead>
         <tbody>
+
         <tr>
           <td>1</td>
           <td>https://www.google.com</td>
           <td>Monitör Askısı</td>
           <td>Monitör Askısı Çok Güzel Hacı ya</td>
-          <td>1</td>
+          <td>
+            <input
+              id="switch-2-2"
+              type="checkbox"
+              data-switchery
+              data-color="#10c469"
+              checked
+            />
+          </td>
           <td>
             <a href="#" class="btn btn-danger btn-xs btn-outline"><i class="fa fa-trash"></i> Sil</a>
             <a href="#" class="btn btn-info btn-xs btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
