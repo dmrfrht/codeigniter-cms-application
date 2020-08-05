8aee82205488ad1c70ce1bf844d86433bc870e1d
Ferhat Demir <dmrfrht1@gmail.com>
2020-08-05 12:25:20 +0300

    css ile urun fotograflarinin tasariminda degisiklikler yapildi

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -22,32 +22,34 @@
   <div class="col-md-12">
     <div class="widget">
       <div class="widget-body">
-        <table class="table table-bordered table-striped table-hover table-responsive">
+        <table class="table table-bordered table-striped table-hover table-responsive picture_list">
           <thead>
-          <th>#id</th>
-          <th>Görsel</th>
+          <th class="w25 text-center">#id</th>
+          <th class="w50 text-center">Görsel</th>
           <th>Resim Adı</th>
-          <th>Durum</th>
-          <th>İşlem</th>
+          <th class="w50 text-center">Durum</th>
+          <th class="w50 text-center">İşlem</th>
           </thead>
           <tbody>
           <tr>
-            <td>1</td>
+            <td class="text-center">1</td>
             <td>
-              <img width="30" src="https://st2.myideasoft.com/shop/rc/57/myassets/products/582/canon-eos-r-24-105mm-lens-1_min.jpg?revision=1536233567" alt="" class="img-responsive">
+              <img width="30"
+                   src="https://st2.myideasoft.com/shop/rc/57/myassets/products/582/canon-eos-r-24-105mm-lens-1_min.jpg?revision=1536233567"
+                   alt="" class="img-responsive">
             </td>
             <td>Resim 1</td>
-            <td>
+            <td class="text-center">
               <input
                 class="isActive"
-                data-url="<?= base_url("product/isActiveSetter/")  ?>"
+                data-url="<?= base_url("product/isActiveSetter/") ?>"
                 type="checkbox"
                 data-switchery
                 data-color="#10c469"
                 <?= (true) ? " checked" : null ?>
               />
             </td>
-            <td>
+            <td class="text-center">
               <button
                 data-url="<?= base_url("product/delete/") ?>"
                 class="btn btn-danger btn-xs btn-outline remove-btn">
